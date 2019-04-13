ffdd1b598366bc223706ae243356c4caf70dd1d0
kkpol1 <konradpietapl@gmail.com>
2019-04-13 11:57:43 +0200

    Fixed git foult

--- src/Test/ThreeDPoint.java
+++ src/Test/ThreeDPoint.java
@@ -1,4 +1,28 @@
 package Test;
 
-public class ThreeDPoint {
+public class ThreeDPoint extends Point {
+
+    private int z;
+
+    public ThreeDPoint(int x, int y, int z){
+            super(x, y);
+            this.z = z;
+    }
+
+    @Override
+    protected String location() {
+            return super.location() + ", " + z;    // printing as "x, y, z"
+    }
+
+    @Override
+    public int manhattanDistanceFromOrigin() {
+            // first, we ask the superclass for the distance of x+y
+            // and then we add the value of z to it
+         return super.manhattanDistanceFromOrigin() + Math.abs(z);
+    }
+
+//    @Override
+//    public String toString() {
+//            return "(" + this.location() + ") distance " + this.manhattanDistanceFromOrigin();
+//    }
 }
